404e6eb899832997933fdff38ae0f0b589f16de4
Andy <121941283+Andytomsingvien@users.noreply.github.com>
2023-06-01 09:24:49 +0200

    Initial commit

---
+++ src/main/java/a/tsv/projetcafe/ProjetCafeApplication.java
@@ -0,0 +1,13 @@
+package a.tsv.projetcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/a/tsv/projetcafe/ProjetCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package a.tsv.projetcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
